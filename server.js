7ca95e882c5f448d539bfa3b2411294f69638f6a
depank <30722799+depank@users.noreply.github.com>
2017-08-21 16:09:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -181,33 +181,9 @@ var htmlTemplet=`
               <input type="button" value="submit" id="submit_btn">
               </div>
               <div>
-              <ul id="list_cmnt"><li>ainooa</li></ul></div>
+              <ul id="list_cmnt"></ul></div>
      <script src="/ui/main.js" rel="script/javascript"></script>
-     <script>
-     console.log("hjbvhj");
-var submit_btn=document.getElementById('submit_btn');
-submit_btn.onclick=function (){
-console.log("hjbvhjjk1");
-
-var coment=document.getElementById('comment').value;
-var req=new XMLHttpRequest();
-req.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       var  list="";
-       var cmnt_=JSON.parse(req.responseText);
-       console.log(cmnt_);
-       for(var i=0;i<cmnt_.length;i++){
-       list +="<li>"+cmnt_[i]+"</li>";}
-    }
-    console.log(list);
-    var cmnt_list=document.getElementById("list_cmnt");
-    cmnt_list.innerHTML=list;
-};
-req.open("GET", "/submit/"+coment, true);
-
-req.send();
-};</script>
+ 
 </div>
 </body>
 </html>
