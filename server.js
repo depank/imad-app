7784f425d8f5d91d16ef4d0f1525d128a6b153d8
depank <30722799+depank@users.noreply.github.com>
2017-08-21 23:40:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -199,9 +199,7 @@ app.get('/submit',function(req,res){
        if(err){
            res.status(500).send(err.toString());
        }
-       else if(result.rows.length === 0){
-           res.status(404).send("Article not found");
-       }
+      
        else{
            //res.send(result.rows[0].comment);
            
