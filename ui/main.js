97071f63b49ae574bb31b1b24d370eb02b6ff915
depank <30722799+depank@users.noreply.github.com>
2017-08-21 14:20:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
-alert("hii,its deepak.");
+var submit_btn=document.getElementById('submit_btn').value;
+submit_btn.onclick=function (){
+
+
+var coment=document.getElementById('comment').value;
+var req=new XmlHttpRequest();
+req.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       var cmnt_=JSON.parse(req.responseText);
+       for(int i=0;i<cmnt_.length;i++){
+       document.getElementById("list_cmnt").innerHTML +="<li>"+cmnt_[i]+"</li>";}
+    }
+};
+req.open("GET", "/submit-'"+coment+"'", true);
+req.send();
+};
