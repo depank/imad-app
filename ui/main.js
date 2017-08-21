7699d0c3620c3691f0192230e28720782b0017ad
depank <30722799+depank@users.noreply.github.com>
2017-08-21 16:09:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log("hjbvhj");
+    console.log("hjbvhj");
 var submit_btn=document.getElementById('submit_btn');
 submit_btn.onclick=function (){
 console.log("hjbvhjjk1");
@@ -8,14 +8,15 @@ var req=new XMLHttpRequest();
 req.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
-       list="";
+       var  list="";
        var cmnt_=JSON.parse(req.responseText);
+       console.log(cmnt_);
        for(var i=0;i<cmnt_.length;i++){
        list +="<li>"+cmnt_[i]+"</li>";}
     }
     console.log(list);
-    var cmnt_list=document.getElementById("ist_cmnt");
-    cmnt_list.innerHtml=list;
+    var cmnt_list=document.getElementById("list_cmnt");
+    cmnt_list.innerHTML=list;
 };
 req.open("GET", "/submit/"+coment, true);
 
