f08ed9f3f38df45b2c37cde7571f4adf14e344fe
depank <30722799+depank@users.noreply.github.com>
2017-08-21 06:28:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -207,7 +207,7 @@ pool.query('SELECT * FROM test',function(err,result){
 });
 
 app.get('/articles/:articleName',function(req,res){
-   pool.query("SELECT * FROM article  WHERE title= $1"+[req.params.articleName],function(err,result){
+   pool.query("SELECT * FROM article  WHERE title= $1",[req.params.articleName],function(err,result){
        if(err){
            res.status(500).send(err.toString());
        }
