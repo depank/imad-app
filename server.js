5510645f49fb6ba2cb27ed7393ad9c7c9126bc71
depank <30722799+depank@users.noreply.github.com>
2017-08-21 06:14:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -206,9 +206,20 @@ pool.query('SELECT * FROM test',function(err,result){
 });
 });
 
-app.get('/:articleName',function(req,res){
-   var articleName=req.params.articleName;
-    res.send(createTemplet(articles[articleName]));
+app.get('/articles/:articleName',function(req,res){
+   pool.query("SELECT * FROM article  WHERE title= '"+req.params.articleName+"'",function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       }
+       else if(result.rows.length === 0){
+           res.status(404).send("Article not found");
+       }
+       else{
+           var articleData=result.rows[0];
+            res.send(createTemplet(articleData));
+       }
+   });
+   
 });
 
 
