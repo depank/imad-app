334569d07fcbeae8ba1b8e37146f0175a099b34a
depank <30722799+depank@users.noreply.github.com>
2017-10-05 22:03:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,6 +104,13 @@ app.get('/new_isro/',function(req,res){
 app.get('/isro/send', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'isro','send.html'));
 });
+app.get('/isro/script/send2.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'isro','script''send2.js'));
+});
+
+app.get('/isro/css/send.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'isro','css''send.css'));
+} 
 
 app.get('/ui/isro/contact.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'isro','contact.html'));
